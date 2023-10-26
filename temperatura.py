15761b02c169b4c03481532f9bab31c600d02a74
RyperoneR <145667779+RyperoneR@users.noreply.github.com>
2023-10-26 12:10:57 +0200

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,5 @@
+temperatura_celsius = float(input("Temperatura en Celsius: "))
+
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+
+print ("La temperatura en F:" , temperatura_fahrenheit)
