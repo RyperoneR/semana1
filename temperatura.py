45ac45bdb7a9e7bcbf22c7079160ea27ee3c0989
RyperoneR <145667779+RyperoneR@users.noreply.github.com>
2023-10-26 12:13:26 +0200

    Delete temperatura.py

--- temperatura.py
+++
@@ -1,5 +0,0 @@
-temperatura_celsius = float(input("Temperatura en Celsius: "))
-
-temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
-
-print ("La temperatura en F:" , temperatura_fahrenheit)
